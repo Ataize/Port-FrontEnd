02efcde57c0730063dcb63472f74a79301f15c8b
Ataize Feitosa <isa.aragao@hotmail.com>
2022-03-25 11:06:42 -0300

    Atualização portfólio

--- validacao.js
+++
@@ -1,9 +0,0 @@
-function validarDados() {
-	window.event.preventDefault()
-
-	if(document.form.nome.value == "") {
-		alert("Campo é obrigatório")
-		document.form.nome.focus()
-	}
-}
-document.querySelector("form").addEventListener("submit", validarDados)
